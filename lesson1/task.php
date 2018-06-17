900f490c46db0487e1bfb0ae28afba5aa3c6de7e
fpestunov <fpestunov@gmail.com>
2018-06-17 22:11:30 +0400

    Add Complete() method

--- lesson1/task.php
+++ lesson1/task.php
@@ -2,14 +2,21 @@
 class Task
 {
     public $description;
+    public $completed = false;
     public function __construct($description)
     {
         $this->description = $description;
+    }
+    public function complete()
+    {
+        $this->completed = true;
     }
 }
 
 $task = new Task('Go to the store'); // First instance
 $task2 = new Task('Learn OOP'); // Second instance
 
-var_dump($task->description);
-var_dump($task2->description);
+$task2->complete();
+
+var_dump($task);
+var_dump($task2);
