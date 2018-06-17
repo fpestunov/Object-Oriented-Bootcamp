42758957af53273430ae298157f1f948dac5f4c1
fpestunov <fpestunov@gmail.com>
2018-06-17 22:59:49 +0400

    Add Person class

---
+++ lesson2/Person.php
@@ -0,0 +1,17 @@
+<?php
+
+class Person
+{
+    public $name;
+    public $age;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+}
+
+$john = new Person('John Doe');
+$john->age = 30;
+
+var_dump($john);
