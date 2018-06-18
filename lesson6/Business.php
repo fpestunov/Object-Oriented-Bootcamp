cf5981263a428b58974fd40a87c54fd2bd14cbb6
fpestunov <fpestunov@gmail.com>
2018-06-18 22:26:47 +0400

    Include files by require

--- lesson6/Business.php
+++ lesson6/Business.php
@@ -1,59 +1,8 @@
 <?php
 
-class Person
-{
-	protected $name;
-
-	public function __construct($name) // Default variable
-	{
-		$this->name = $name;
-	}
-
-}
-
-class Business
-{
-	protected $staff;
-
-	public function __construct(Staff $staff)
-	{
-		$this->staff = $staff;
-	}
-	// What are doing business? Hire()
-	// Who hire business? $person
-	public function hire(Person $person)
-	{
-		// Add $person to the Staff collection
-		// if you write method the you should add it in Staff
-		$this->staff->add($person); // way objects communicate with other
-	}	
-
-	public function getStaffMembers()
-	{
-		return $this->staff->members(); // way objects communicate with other
-	}	
-
-}
-
-class Staff
-{
-	protected $members = [];
-
-	public function __construct($members = [])
-	{
-		$this->members = $members;
-	}
-
-	public function add(Person $person)
-	{
-		$this->members[] = $person;
-	}
-
-	public function members()
-	{
-		return $this->members;
-	}
-}
+require 'src/Person.php';
+require 'src/Staff.php';
+require 'src/Business.php';
 
 $jeffrey = new Person('Jeffry Way');
 $staff = new Staff([$jeffrey]);
